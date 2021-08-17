7cdb07b15db8dbeda98f65cc26b4fdd7283a9063
Cherod Joseph <jcherod365@gmail.com>
2021-08-17 18:33:16 +0200

    my first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import csv
+import requests
+
+from bs4 import BeautifulSoup
+
+
+# every categories
+
+get_categories()
+
+
+
+
+
+# the books "thriller"
+
+books_of_categories_thriller()
+
+
+
+
+# tous les elements de la liste pour un livre
+
+get_product_info(url3 = "http://books.toscrape.com/catalogue/killing-floor-jack-reacher-1_382/index.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
