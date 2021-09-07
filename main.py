0c143b22f1ed020c25aae721d27873b6bae967cd
Cherod Joseph <jcherod365@gmail.com>
2021-09-07 21:31:40 +0200

    les modifs

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
+
 from utils import  (
     get_categories, get_jpg, books_of_category, get_product_info,
     get_books_links_one_categorie, get_images_titles_urls_product_info_of_one_categorie,
-    get_url_category
+    get_url_category, get_pictures
 )
 
 
-from pictures import get_imagejpg
 
+#GET PRODUCT INFO ONE BOOK
+products_infos = get_product_info('https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html')
+print(products_infos)
+
+#GET ALL LINKS BOOKS ONE CATEGORIE (TRAVELD)
+books_links = get_books_links_one_categorie()
+for link in books_links:
+    print('URL:' , link) 
+
+#GET PRODUCT INFO ALL BOOKS ONE CATEGORIE
+infos_books = get_products(books_links)
 
 # RETRIEVE ALL CATEGORIES
 categories = get_categories()
 for category in categories:
     print('category:', category)
 
-# #LIST PICTURE ONE CATEGORIE (TRAVELD)
-# list_jpg_travel_categorie = get_jpg()
-
+#LIST PICTURE ONE CATEGORIE (TRAVELD)
+list_jpg_travel_categorie = get_jpg()  
 
 # GET EVERY BOOKS OF every categories
 for category_url in categories:
-    books = books_of_category(category_url)
+    books = books_of_category(category_url)  
+
+#GET ADRESS URL OF EACH CATEGORY
+list_adress_category = get_url_category()    
+
+#GET PICTURES
+pictures = get_pictures()
 
 
-# #GET PRODUCT INFO ONE BOOK
-# list_product_info = get_product_info('https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html')
 
 
-# #GET BOOKS LINKS ONE CATEGORIE (TRAVELD)
-# list_books_links_one_categorie = get_books_links_one_categorie()
 
-# #GET PRODUCT INFO ALL BOOKS ONE CATEGORIE
-# list_product_info_books_one_categorie = get_images_titles_urls_product_info_of_one_categorie()
 
-# #GET ADRESS URL OF EACH CATEGORY
-# list_adress_category = get_url_category()
+
+
+
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,18 +1,155 @@
+from bs4 import BeautifulSoup
 import csv
-
 import requests
 
-from bs4 import BeautifulSoup
+
+
+"""
+Choisissez n'importe quelle page Produit sur le site de Books to Scrape. Écrivez un script Python qui visite cette page et en extrait les informations suivantes :
+
+product_page_url
+universal_ product_code (upc)
+title
+price_including_tax
+price_excluding_tax
+number_available
+product_description
+category
+review_rating
+image_url
+
+Écrivez les données dans un fichier CSV qui utilise les champs ci-dessus comme en-têtes de colonnes.
+"""
+
+
+def get_product_info(category_info ):
+  
+#Return product info of one book
+      
+  url1 = category_info
+  page1 = requests.get(url1)
+  soup1 = BeautifulSoup(page1.content,'html.parser')
+
+  product_info = []
+           
+  title = soup1.find('h1')
+  categorie = soup1.find('ul',{'class':'breadcrumb'}).findAll('li')
+  category = categorie[2].text.replace("\n"," ")
+  ratings = soup1.findAll('p')
+  review_rating = ratings[2]['class'] 
+  product_info_keys = soup1.find('table',{"class":"table table-striped"}).findAll('tr')
+  product_info_values = soup1.find('table',{"class":"table table-striped"}).findAll('tr')
+  images = soup1.findAll('div',{'class':'item active'})
+  
+  for i in images:
+        t = i.find('img')
+        w = t['src']
+        image_url = w.replace('../../','https://books.toscrape.com/')
+
+
+  list_keys = ["title","category","review_rating","image_url"] 
+  list_values = [title.text,category,review_rating,image_url]  
+      
+  for product_info_key in product_info_keys:
+            k = product_info_key.find('th')
+            list_keys.append(k.text)    
+    
+  for product_info_value in product_info_values:
+            v = product_info_value.find('td')
+            list_values.append(v.text) 
+
+ 
+  product_info = [
+                    {"title": title.text},
+                    {"category": category},  
+                    {"review_rating" : review_rating[1]}, 
+                    {"image_url" : image_url},
+                    {list_keys[0]: list_values[0]},                
+                    {list_keys[1]: list_values[1]},
+                    {list_keys[2]: list_values[2]},
+                    {list_keys[3]: list_values[3]},
+                    {list_keys[4]: list_values[4]},
+                    {list_keys[5]: list_values[5]},
+                    {list_keys[6]: list_values[6]}
+                  ]
+
+                 
+#File csv          
+
+  articles = ["keys", "values"]
+  with open('infos.csv', 'w') as files_csv:
+     writer = csv.writer(files_csv, delimiter=',')
+     writer.writerow(articles)
+     for product_info_key, product_info_value in zip(list_keys, list_values):         
+       writer.writerow([product_info_key,product_info_value])
+
+  return product_info                 
+
+
+
+"""
+Écrivez un script Python qui consulte la page de la catégorie choisie,
+et extrait l'URL de la page Produit de chaque livre appartenant à cette catégorie
+"""
+
 
 url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
 page = requests.get(url)
 soup = BeautifulSoup(page.content,'html.parser')
 
+def get_books_links_one_categorie(): 
+    lienProductInfo = []
+    liens = soup.findAll('div',{'class':'image_container'})
+    for i in liens:
+        a = i.find('a')
+        link = a['href']
+        lienProductInfo.append(link.replace('../../../','https://books.toscrape.com/catalogue/'))    
+    return lienProductInfo  
 
+
+#extraire les données produit de tous les livres de la catégorie choisie, puis écrivez les données dans un seul fichier CSV.
+    
+
+
+def get_products(lienProductInfo):
+    
+    list_catalogue_product = []
+      
+    for x in range(len(lienProductInfo)):
+          url6 = lienProductInfo[x]  
+          page6 = requests.get(url6)
+          soup6 = BeautifulSoup(page6.content,'html.parser') 
+                  
+          
+          images = soup6.find('div',{'class':'carousel-inner'}).find('div',{'class':'item active'})
+          t = images.find('img')
+          w = t['src']
+
+          titleBook = soup6.find('ul',{'class':'breadcrumb'}).find('li',{'class':'active'}) 
+          books = soup6.find('ul',{'class':'breadcrumb'}).findAll('li') 
+          product = soup6.findAll('th')
+          product2 = soup6.findAll('td')
+          title = soup6.find('h1')
+
+          list_catalogue_product =  [
+                                      {"url page" : lienProductInfo[x]},
+                                      {"url_image" : w.replace('../../','https://books.toscrape.com/')},
+                                      {"title" : titleBook.string},
+                                      {product[0].text: product2[0].text},
+                                      {product[1].text: product2[1].text},
+                                      {product[2].text: product2[2].text},
+                                      {product[3].text: product2[3].text}, 
+                                      {product[4].text: product2[4].text},
+                                      {product[5].text: product2[5].text},
+                                      {product[6].text: product2[6].text} 
+
+                                  ]                                
+
+          for catalogue in list_catalogue_product:
+               print(catalogue)                       
+
+#RETURN A LIST OF EVERY CATEGORIES OF BOOKSCRAP WEBSITE
 def get_categories():
-    """
-    Returns a list of every categories of bookscrap website.
-    """
     f =  soup.find('aside',{'class':'sidebar col-sm-4 col-md-3'}).find('ul').findAll('li')
     categories = []
     for i in range(1, len(f)):
@@ -20,12 +157,9 @@ def get_categories():
         b = a.strip()
         categories.append(b)
     return categories
-    
 
+#RETURN A LIST OF JPG CATEGORY (TRAVELD) 
 def get_jpg():
-    """
-    Return a list of jpg category (travel).
-    """
     images = soup.findAll('div',{'class':'image_container'})
 
     img_list = []  
@@ -38,8 +172,7 @@ def get_jpg():
 
 
 # RETURNS EVERY TITLES OF CATEGORIES TRAVELD
-def books_of_category(category_url):
-    
+def books_of_category(category_url):    
      url = category_url
      titre_categorie_traveld = soup.findAll('h3') 
      for t in titre_categorie_traveld:
@@ -51,162 +184,43 @@ def books_of_category(category_url):
      print("")
      print("")
 
-
-#RETURN PRODUCT INFO D'UN LIVRES D'UNE CATEGORIE (TRAVELD)
-def get_product_info(category_info ):
-    
-    url1 = category_info
-    page1 = requests.get(url1)
-    soup1 = BeautifulSoup(page1.content,'html.parser')
-           
-    title = soup1.find('h1')
-    categorie = soup1.find('ul',{'class':'breadcrumb'}).findAll('li') 
-    rating = soup1.find("div",{'class':'col-sm-6 product_main'})
-    ratings = str(rating)[229:234].replace('"','') + "/five"
-    product_info_keys = soup1.find('table',{"class":"table table-striped"}).findAll('tr')
-    product_info_values = soup1.find('table',{"class":"table table-striped"}).findAll('tr')
-
-    list_keys = [] 
-    list_values = []    
-    
-    for product_info_key in product_info_keys:
-        k = product_info_key.find('th')
-        list_keys.append(k.text)    
-    
-    for product_info_value in product_info_values:
-        v = product_info_value.find('td')
-        list_values.append(v.text) 
-
-    
-    product_info = {
-                       "title": title.text,
-                       "category": categorie[2].text.replace("\n"," "),  
-                       "rating" : ratings, 
-                       list_keys[0]: list_values[0],                
-                       list_keys[1]: list_values[1],
-                       list_keys[2]: list_values[2],
-                       list_keys[3]: list_values[3],
-                       list_keys[4]: list_values[4],
-                       list_keys[5]: list_values[5],
-                       list_keys[6]: list_values[6]
-                    }
-
-    print("")
-    print("")
-    print(product_info) 
-
-
-  
-
-"""
-
-header = ['product_info_key','product_info_value']
-with open('case.csv','w') as list_file:
-    writer = csv.writer(list_file, delimiter = ',')
-    writer.writerow(header)
-    for product_info_key, product_info_value in zip(list_keys, list_values):
-        writer.writerow([product_info_key,product_info_value])
-        
-"""
-
-
-def get_books_links_one_categorie(): 
-    lienProductInfo = []
-
-    liens = soup.findAll('div',{'class':'image_container'})
-    for i in liens:
-        a = i.find('a')
-        link = a['href']
-        lienProductInfo.append(link.replace('../../../','https://books.toscrape.com/catalogue/'))
-    print("\n".join(lienProductInfo))    
-
-
-def get_images_titles_urls_product_info_of_one_categorie(): 
-
-    for x in range(10):
-
-      lienProductInfo = []
-
-      liens = soup.findAll('div',{'class':'image_container'})
-      for i in liens:
-            a = i.find('a')
-            link = a['href']
-            lienProductInfo.append(link.replace('../../../','https://books.toscrape.com/catalogue/'))
-
-      url6 = lienProductInfo[x]  
-      page6 = requests.get(url6)
-      soup6 = BeautifulSoup(page6.content,'html.parser')
-
-  
-      titleBook = soup6.find('ul',{'class':'breadcrumb'}).find('li',{'class':'active'}) 
-
-      images = soup6.find('div',{'class':'carousel-inner'}).find('div',{'class':'item active'})
-      t = images.find('img')
-      w = t['src']
-
-  
-      print("")
-      print("") 
-      print("images: ",w.replace('../../','https://books.toscrape.com/'))#images
-      print("")
-      print("")  
-      print("titres: ",titleBook.string)#titres
-      print("")
-      print("")  
-      print("url pages: ",lienProductInfo[x])#url de la page du livre
-      print("")
-      print("") 
-
-
-      print("")
-      print("")
-      print("")
-      print("")
-      print("")
-      print("") 
-
-
-    for y in range(10):  
-
-      url6 = lienProductInfo[y]  
-      page6 = requests.get(url6)
-      soup6 = BeautifulSoup(page6.content,'html.parser')      
-  
-      books = soup6.find('ul',{'class':'breadcrumb'}).findAll('li') 
-      product = soup6.findAll('th')
-      product2 = soup6.findAll('td')
-      title = soup6.find('h1')
-
-      product_info = {
-                       "title": title.text,                                             
-                       product[0].text: product2[0].text,
-                       product[1].text: product2[1].text,
-                       product[2].text: product2[2].text,
-                       product[3].text: product2[3].text,
-                       product[4].text: product2[4].text,
-                       product[5].text: product2[5].text,
-                       product[6].text: product2[6].text
-                     }
-
-      print("")
-      print("")
-      print(product_info) 
-
-
+# LIST: ADRESS URL OF EACH CATEGORY 
 def get_url_category():
-            
-  # LIST: ADRESS URL OF EACH CATEGORY 
     f =  soup.find('aside',{'class':'sidebar col-sm-4 col-md-3'}).find('ul').findAll('li')
-
     listUrlCategorie = []
     j = 0
 
-    for i in range (2,51):
+    for i in range (len(li)):
         j = j + 1
         a = f[j].text.replace("\n","")
         b = a.strip()
-        url1 = "https://books.toscrape.com/catalogue/category/books/"+str(b)+"_"+str(i)+"/index.html"
+        c = b.lower()
+        url1 = "https://books.toscrape.com/catalogue/category/books/"+str(c)+"_"+str(i)+"/index.html"
         page1 = requests.get(url1)
         soup1 = BeautifulSoup(page1.content,'html.parser')
         listUrlCategorie.append(url1)
         print(*listUrlCategorie,sep="\n")
+  
+  
+    
+# DOWNLOAD PICTURE ONE CATEGORIE(TRAVELD)
+def get_pictures():
+    f = open('picture.jpg','wb')
+    response = requests.get('https://books.toscrape.com/media/cache/6d/41/6d418a73cc7d4ecfd75ca11d854041db.jpg')
+    f.write(response.content)
+    f.close()
+
+
+
+
+
+                            
+
+
+
+
+
+    
+
+
+
