2782c8a97b46b9ef6868edf2790ec3798233f893
Cherod Joseph <jcherod365@gmail.com>
2021-09-15 19:50:06 +0200

    les modifs 2

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 
-from utils1 import  get_url_category, get_books_links_one_categorie, get_products
-
-
-
+from omega import  get_url_category, pagination, get_books_links_one_categorie, get_products
 
 #GET ADRESS URL OF PAGES EACH CATEGORY
 adress_category = get_url_category() 
-#print(list_adress_category)   
+print(adress_category)  
 
 #GET ALL LINKS BOOKS ONE CATEGORIE (TRAVELD)
 books_links = get_books_links_one_categorie(adress_category)
